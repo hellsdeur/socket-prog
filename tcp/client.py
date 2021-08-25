c13b5676e099e6f6fee2574b9409ac65d9501a84
Helder Matos <mtshelder@gmail.com>
2021-08-25 19:58:28 -0300

    Adding some comments

--- tcp/client.py
+++ tcp/client.py
@@ -7,11 +7,13 @@ port = 11234
 # cria objeto socket IPV4/TCP
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+# conectar com servidor remoto
 s.connect((ip, port))
 
-# mensagem de requisição
+# mensagem de requisição inicial
 s.send("Solicitação de novo jogo.".encode("utf-8"))
 
+# respostas iniciais do servidor
 category = s.recv(1024).decode("utf-8")
 hidden = s.recv(1024).decode("utf-8")
 
@@ -19,9 +21,11 @@ print("Categoria: " + category)
 print("Palavra: " + ' '.join(list(hidden)))
 
 while hidden.count('_') > 0:
+	# inserção dos chutes
 	guess = input("Insira a letra ou palavra: ")
 	s.send(guess.encode("utf-8"))
 	
+	# resposta do servidor
 	hidden = s.recv(1024).decode("utf-8")
 
 	print("Palavra: " + ' '.join(list(hidden)))
--- tcp/server.py
+++ tcp/server.py
@@ -6,37 +6,45 @@ import hangman
 ip = socket.gethostname()
 port = 11234
 
-
 # cria objeto socket IPV4/TCP
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+# associando o socket com o endereço
 s.bind((ip, port))
 
 # escutar 5 conexões simultâneas
 s.listen(5)
 
-print(f"Listening {ip}: {port}")
+print(f"Escutando {ip}: {port}")
 
+# tratamento da requisição do cliente
 def handle_client(client_socket):
+	# requisição inicial do cliente
 	request = client_socket.recv(1024)
 	request_decoded = request.decode("utf-8")
 	print(f"Recebeu {request_decoded}")
 	print(f"-------------------------")
 
+	# geração do jogo
 	category, word = hangman.generate_word()
 	word = word.lower()
 	print(f"Palavra: {word}")
 	hidden = "_"*len(word)
 	
+	# resposta inicial do servidor
 	client_socket.send(f"{category}".encode("utf-8"))
 	client_socket.send(f"{hidden}".encode("utf-8"))
 
 
 	while hidden.count('_') > 0:
+		# envio dos chutes do cliente
 		guess = client_socket.recv(1024).decode("utf-8").lower()
+
+		# avaliação do chute
 		if hangman.evaluate(word, hidden, guess):
 			hidden = hangman.update(word, hidden, guess)
 
+		# envio do servidor
 		client_socket.send(f"{hidden}".encode("utf-8"))
 
 	client_socket.close()
